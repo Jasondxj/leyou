3ecf21ac27c74849f74f16652da458b5e9dcc458
Jason <291417099@qq.com>
2019-05-14 14:40:36 +0800

    上传文件到FastDFS

--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,8 +1,14 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.upload.config.UploadProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,15 +21,20 @@ import java.util.List;
 
 @Service
 @Slf4j
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadService {
-    private static final List<String> ALLOW_TYPES= Arrays.asList("image/jpeg","image/png","image/bpm");
+    @Autowired
+    private FastFileStorageClient storageClient;
+    @Autowired
+    private UploadProperties prop;
+//    private static final List<String> ALLOW_TYPES= Arrays.asList("image/jpeg","image/png","image/bpm");
     public String uploadImage(MultipartFile file) {
-        File dest=new File("D:\\upload",file.getOriginalFilename());
+//        File dest=new File("D:\\upload",file.getOriginalFilename());
         //保存文件到本地
         try {
             //校验文件类型
             String contentType = file.getContentType();
-            if (!ALLOW_TYPES.contains(contentType)){
+            if (!prop.getAllowTypes().contains(contentType)){
                 throw new LyException(ExceptionEnum.FILE_TYPE_ERROR);
             }
             //校验文件内容
@@ -31,9 +42,12 @@ public class UploadService {
             if (image==null){
                 throw new LyException(ExceptionEnum.FILE_TYPE_ERROR);
             }
-            file.transferTo(dest);
+            //上传到FastDFS
+//            String extension=file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+            String extension= StringUtils.substringAfterLast(file.getOriginalFilename(),".");
+            StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
             //返回路径
-            return "http://image.leyou.com/"+file.getOriginalFilename();
+            return prop.getBaseUrl()+storePath.getFullPath();
         } catch (IOException e) {
             log.error("上传文件失败",e);
             throw new LyException(ExceptionEnum.FILE_UPLOAD_FAIL);
